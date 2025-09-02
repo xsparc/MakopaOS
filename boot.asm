5254d21cd2631aa4631cd8b9d77272009049246e
Louijie Compo <louijie.compo@gmail.com>
2025-09-02 03:32:09 +0000

    Utilized stack segment register.
    Signed-off-by: Louijie Compo <louijie.compo@gmail.com>

--- boot.asm
+++ boot.asm
@@ -1,14 +1,23 @@
 ; Bootloader entry point for MakopaOS
 ; This code prints 'MAKOPA' to the screen using BIOS interrupt 0x10
 
-ORG 0x7c00                ; Set code origin to 0x7C00 (where BIOS loads the boot sector)
-BITS 16                   ; 16-bit real mode
+ORG 0                     ; Set code origin to 0x7C00 (where BIOS loads the boot sector)
+BITS 16                   ; 16-bit real mode (8086 compatible)
 
 start:
-    mov si, message       ; SI points to the start of the message string
+    cli                   ; Disable interrupts during setup for safety
+    mov ax, 0x7c0         ; Set segment registers to 0x7C00 (boot sector load address >> 4)
+    mov ds, ax            ; Data Segment (DS) points to bootloader code/data
+    mov es, ax            ; Extra Segment (ES) also points to bootloader
+    mov ax, 0x00          ; Stack Segment (SS) set to 0x0000
+    mov ss, ax            ; Set SS to 0x0000
+    mov sp, 0x7c00        ; Set Stack Pointer (SP) to 0x7C00 (top of boot sector)
+    sti                   ; Re-enable interrupts after setup
+    
+    mov si, message       ; Load address of message string into SI for printing
 
 print_char:
-    mov ah, 0eh           ; BIOS teletype function (int 0x10, ah=0Eh)
+    mov ah, 0eh           ; BIOS teletype output function (int 0x10, ah=0Eh)
     lodsb                 ; Load byte at [SI] into AL, increment SI
     cmp al, 0             ; Check if end of string (null terminator)
     je hang               ; If zero, jump to hang (infinite loop)
@@ -16,9 +25,11 @@ print_char:
     jmp print_char        ; Repeat for next character
 
 hang:
-    jmp hang              ; Infinite loop to halt execution
+    jmp hang              ; Infinite loop to halt execution (system halt)
 
 message db 'MAKOPA', 0    ; Message string, null-terminated
 
-times 510-($ - $$) db 0   ; Pad the rest of the sector with zeros
-dw 0xAA55                 ; Boot sector signature (must be 0xAA55)
+times 510-($ - $$) db 0   ; Pad the rest of the sector with zeros to make 512 bytes
+
+; Boot sector signature (must be 0xAA55 for BIOS to recognize as bootable)
+dw 0xAA55
